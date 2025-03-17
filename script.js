5f8007b700910be21b4fc582fef3b8312e7405e8
svesh20 <sveshnikov020@gmail.com>
2025-03-17 17:59:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-// Функция для смены страниц
 function showPage(pageId) {
-    // Скрываем все страницы
     document.querySelectorAll('.page').forEach(page => {
         page.style.display = 'none';
     });
-    // Показываем выбранную страницу
     document.getElementById(pageId).style.display = 'block';
 }
-
-// По умолчанию показываем первую страницу
 showPage('about');
 
